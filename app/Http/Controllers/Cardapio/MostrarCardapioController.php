de136e09503b56561e5b4b85554261c4ea3889e3
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-09-23 18:56:13 -0300

    Atualizado, obter produtos

--- app/Http/Controllers/Cardapio/MostrarCardapioController.php
+++ app/Http/Controllers/Cardapio/MostrarCardapioController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Cardapio;
 
 use App\Http\Controllers\Controller;
 use App\Models\Empresa;
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class MostrarCardapioController extends Controller
 {
     public function show(Request $request, $slug){
         $empresa = Empresa::where('slug_nome_empresa', $slug)->first();
-        dd($empresa->getAttributes());
+        $id_empresa = $empresa->getAttributes()['id'];
+
+        $produtos = Produto::where('fk_empresa', $id_empresa)->get();
+        for($i=0;$i<=$produtos->count();$i++){
+            dd($produtos[$i]->getAttributes());
+        }
     }
 }
