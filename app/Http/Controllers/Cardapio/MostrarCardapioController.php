d741df59f33063e708d215216ff9deb11bb71879
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-09-24 13:12:34 -0300

    Adicionado cache redis nas querys sql

--- app/Http/Controllers/Cardapio/MostrarCardapioController.php
+++ app/Http/Controllers/Cardapio/MostrarCardapioController.php
@@ -6,13 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\Empresa;
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class MostrarCardapioController extends Controller
 {
     public function show(Request $request, $slug)
     {
-        $empresa = Empresa::where('slug_nome_empresa', $slug)->firstOrFail();
-        $produtos = Produto::where('fk_empresa', $empresa->id)->get();
-        return view('cardapio.mostrar', compact('empresa', 'produtos'));
+        $empresa = Cache::remember("empresa_$slug", 60, function () use ($slug) {
+            return Empresa::where('slug_nome_empresa', $slug)->firstOrFail();
+        });
+
+        $produtos = Cache::remember("produtos_da_empresa_ $empresa->id", 60, function () use ($empresa) {
+            return Produto::where('fk_empresa', $empresa->id)->get();
+        });
+
+        return view('cardapio.mostrar', with(['empresa' => $empresa, 'produtos' => $produtos]));
     }
 }
